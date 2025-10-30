365f21e76a6b475cf8520ddc6168f994300397b9
Dr. Tomer Chen <104824283+Tomerlivechen@users.noreply.github.com>
2025-10-30 15:31:44 +0200

    added whidth incres to table name

--- src/components/task-list/task-list-table.tsx
+++ src/components/task-list/task-list-table.tsx
@@ -69,10 +69,10 @@ export const TaskListTableDefault: React.FC<{
           >
             <div
               className={styles.taskListCell}
-              style={{
-                minWidth: rowWidth,
-                maxWidth: rowWidth,
-              }}
+                style={{
+    width: "fit-content",
+    whiteSpace: "nowrap",
+  }}
               title={t.name}
             >
               <div className={styles.taskListNameWrapper}>
