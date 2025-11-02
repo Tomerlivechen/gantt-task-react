75267470549c0ad28c6d7ac2f779229ba02f4751
Dr. Tomer Chen <104824283+Tomerlivechen@users.noreply.github.com>
2025-11-02 13:20:10 +0200

    changing date structure

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -286,11 +286,7 @@ export const Calendar: React.FC<CalendarProps> = ({
         </text>
       );
       if (i === 0 || date.getDate() !== dates[i - 1].getDate()) {
-        const topValue = `${getLocalDayOfWeek(
-          date,
-          locale,
-          "narrow"
-        )}, ${date.getDate()} ${getLocaleMonth(date, locale)}`;
+        const topValue = `${date.getDate()} ${getLocaleMonth(date, locale)}`;
         topValues.push(
           <TopPartOfCalendar
             key={topValue + date.getFullYear()}
